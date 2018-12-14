268c711eb3465881e91a591c17957a17012f6f87
lizagrishkova <grishkova.ea@phystech.edu>
2018-12-14 14:38:43 +0300

    Добавили фон, исправили соударения

--- ground.py
+++ ground.py
@@ -1,51 +1,67 @@
 from tkinter import *
 import random
-import math
+from math import *
 
 screen_width = 800
 screen_height = 600
-root = Tk()
-frame = Frame(root)
-root.geometry('800x600')
-canv = Canvas(root, bg='white')
-canv.pack(fill=BOTH, expand=1)
 
 
 class Background:
     def __init__(self, canvas):
-        self.canvas = canvas
+        self._canvas = canvas
         self.draw()
 
     def draw(self):
-        # Рисуем фон
-        self.canvas.create_rectangle(0, 0, screen_width, screen_height, fill='skyblue')
+        """
+            Рисуем фон
+        """
+        self._canvas.create_rectangle(0, 0, screen_width, screen_height, fill='skyblue')
 
         # Drawing mountains
-        self.canvas.create_polygon((0, 600), (0, 350), (175, 50), (550, 600), fill='slate gray')
-        self.canvas.create_polygon((300, 600), (175, 50), (550, 600), fill='light slate gray')
+        self._canvas.create_polygon((0, 600), (0, 350), (175, 50), (550, 600), fill='slate gray')
+        self._canvas.create_polygon((300, 600), (175, 50), (550, 600), fill='light slate gray')
 
         # Drawing hill
-        self.canvas.create_oval(-100, 800, 600, 350,  fill='forest green', outline='forest green')
+        self._canvas.create_oval(-100, 800, 600, 350,  fill='green4', outline='green4')
 
         # Drawing mountains
-        self.canvas.create_polygon((350, 600), (520, 280), (550, 500), (500, 600), fill='light slate gray')
-        self.canvas.create_polygon((550, 500), (520, 280), (600, 400), fill='slate gray')
+        self._canvas.create_polygon((350, 600), (520, 280), (550, 500), (500, 600), fill='light slate gray')
+        self._canvas.create_polygon((550, 500), (520, 280), (600, 400), fill='slate gray')
 
-        self.canvas.create_polygon((500, 600), (700, 150), (750, 600), fill='light slate gray')
-        self.canvas.create_polygon((750, 600), (700, 150), (800, 300), (800, 600), fill='slate gray')
+        self._canvas.create_polygon((500, 600), (700, 150), (750, 600), fill='light slate gray')
+        self._canvas.create_polygon((750, 600), (700, 150), (800, 300), (800, 600), fill='slate gray')
 
         # Drawing snow
-        self.canvas.create_polygon((115, 150), (175, 50), (185, 100), fill='white smoke')
-        self.canvas.create_polygon((272, 190), (175, 50), (185, 100), fill='snow')
+        self._canvas.create_polygon((115, 150), (175, 50), (185, 100), fill='white smoke')
+        self._canvas.create_polygon((272, 190), (175, 50), (185, 100), fill='snow')
 
-        self.canvas.create_polygon((632, 300), (700, 150), (710, 260), fill='snow')
-        self.canvas.create_polygon((710, 260), (700, 150), (780, 270), fill='white smoke')
+        self._canvas.create_polygon((632, 300), (700, 150), (710, 260), fill='snow')
+        self._canvas.create_polygon((710, 260), (700, 150), (780, 270), fill='white smoke')
 
-        self.canvas.create_polygon((465, 380), (520, 280), (530, 350), fill='snow')
-        self.canvas.create_polygon((530, 350), (520, 280), (587, 380), fill='white smoke')
+        self._canvas.create_polygon((465, 380), (520, 280), (530, 350), fill='snow')
+        self._canvas.create_polygon((530, 350), (520, 280), (587, 380), fill='white smoke')
 
         # Drawing sun
-        self.canvas.create_oval(550, 70, 600, 20, fill='yellow', outline='yellow')
+        self._canvas.create_oval(550, 70, 600, 20, fill='yellow', outline='yellow')
+
+        # Drawing trees
+        x = 100
+        y = 370
+        for _ in range(3):
+            self._canvas.create_line(x, y, x, y + 40, fill='brown', width=10)
+            x += 90
+            y -= 5
+
+        x = 50
+        y = 300
+
+        for _ in range(3):
+            for _ in range(3):
+                self._canvas.create_polygon((x, y), ((2*x + 100)/2, y - 60), (x + 100, y),
+                                           fill='dark green', outline='dark green')
+                y += 45
+            x += 90
+            y -= 150
 
 
 class Ground:
@@ -69,21 +85,21 @@ class Ground:
             height += dh
 
     """
-        Функция отрисовывает землю по списку высот
+         Функция отрисовывает землю по списку высот
     """
     def draw(self):
-        # ыself.canvas.create_rectangle(0, 0, 800, 600, fill='white')
+        # self.canvas.create_rectangle(0, 0, 800, 600, fill='white')
         self.canvas.delete('ground')
         for i in range(800):
             self.canvas.create_line(i, screen_height, i, self.height[i], width=5, fill='lime green', tag='ground')
     """
-        Функция проверяет столкновение снаяряда с землей
+         Функция проверяет столкновение снаяряда с землей
     """
     def check_collision(self, shell):
         return (screen_height - (shell.y - shell.r)) <= self.height[round(shell.x)]
 
     """
-        Функция уменьшает координаты столбиков земли в радиусе поражения снаряда
+         Функция уменьшает координаты столбиков земли в радиусе поражения снаряда
     """
     def explode(self, shell):
         damage = shell.r*1.5
@@ -91,15 +107,9 @@ class Ground:
         right_x = round(min(screen_width, round(shell.x + damage)))
         for i in range(left_x, right_x):
             if i != round(shell.x):
-                dx = abs(shell.x - i)  # расстояние от точки удара до i столбика земли
-                dy = (damage**2 - dx**2)**0.5
+                dx = abs(round(shell.x) - i)  # расстояние от точки удара до i столбика земли
+                dy = ((abs(damage*damage - dx*dx))**0.5)
                 self.height[i] = round(self.height[round(shell.x)] + dy)
         self.height[round(shell.x)] += damage
         self.draw()
-
-
-a = Background(canv)
-gr = Ground(canv)
-gr.draw()
-
-mainloop()
+        shell.destroy()
