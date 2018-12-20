2d69834626d4189623735aa789dd763ac312654c
lizagrishkova <grishkova.ea@phystech.edu>
2018-12-20 18:47:42 +0300

    Добавлена обработка попадания снаряда в пушку; Изменено изображение пушки

--- cannon/cannon.py
+++ cannon/cannon.py
@@ -5,9 +5,14 @@ from random import *
 screen_width = 800
 screen_height = 600
 shell_radius = 10
-dt = 0.1  # физический шаг времени между кадрами обсчёта
+cannon_radius = 20
+dt = 0.3  # физический шаг времени между кадрами обсчёта
 g = -9.8
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'gray', 'black', 'cyan', 'pink', 'magenta']
+
+
+def screen(x, y):
+    return x, screen_height - y
 
 
 class Ball:
@@ -17,7 +22,7 @@ class Ball:
         self.x, self.y, self.r = x, y, r
         self.Vx, self.Vy = Vx, Vy
         self._canvas = canvas
-        self.circle = canvas.create_oval(x - r, (y - r), x + r, (y + r), fill=self.color)
+        self.circle = canvas.create_oval(screen(x - r, y - r), screen(x + r, (y + r)), fill=self.color)
         self.damage_radius = 40
         self.damage = 10
 
@@ -31,8 +36,10 @@ class Ball:
         ay = g
         self.x += self.Vx * dt
         self.y += self.Vy * dt
-        self.Vx += ax * dt
+        self.Vx += (ax * dt)*0.99
         self.Vy += ay * dt
+
+        # отражение
         if self.x - self.r <= 0:
             self.Vx = -self.Vx
             self.x = self.r + 1
@@ -43,13 +50,13 @@ class Ball:
             self.Vy = -self.Vy
             self.y = self.r + 1
 
-        x1, y1 = self.x - self.r, screen_height - (self.y - self.r)
-        x2, y2 = self.x + self.r, screen_height - (self.y + self.r)
+        x1, y1 = screen(self.x - self.r, self.y - self.r)
+        x2, y2 = screen(self.x + self.r, self.y + self.r)
         self._canvas.coords(self.circle, x1, y1, x2, y2)
 
-   # def check_collision(self, x, y):
-    #    l = ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
-     #   return l <= self.r
+    def check_collision(self, x, y):
+       length = ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
+       return length <= self.r
 
     def destroy(self):
         self._canvas.delete(self.circle)
@@ -62,24 +69,31 @@ class Cannon:
         self._canvas = canvas
         # координаты на экране
         self.x, self.y = x, y
+        self.r = cannon_radius
         # self.power = 10
         # self.on = 0
         # self.angle = 1
         self.length_x = 0
         self.length_y = -20
-        self.cannon = self._canvas.create_line(self.x, screen_height - self.y,
-                                               self.x + self.length_x, screen_height - (self.y + self.length_y, width=7, fill='black', tag='cannon')
+        self.score = 0
+        self.health = 100
+        self.cannon = self._canvas.create_line(screen(self.x, self.y,),
+                                               screen(self.x + self.length_x, self.y + self.length_y),
+                                               width=7, fill='black', tag='cannon')
+        self.cannon_body = self._canvas.create_oval(screen(self.x - self.r, self.y - self.r),
+                                                    screen(self.x + self.r, self.y + self.r),
+                                                    fill='black', tag='cannon')
 
     def target(self, x, y):
         # получает экранные координаты
         self.length_x = (x - self.x)
         self.length_y = (y - self.y)
-        l = (self.length_x ** 2 + self.length_y ** 2) ** 0.5
-        self.length_x = self.max_cannon_length * self.length_x / l
-        self.length_y = self.max_cannon_length * self.length_y / l
+        length = (self.length_x ** 2 + self.length_y ** 2) ** 0.5
+        self.length_x = self.max_cannon_length * self.length_x / length
+        self.length_y = self.max_cannon_length * self.length_y / length
 
-        x1, y1 = self.x, screen_height - self.y
-        x2, y2 = self.x + self.length_x, screen_height - (self.y + self.length_y)
+        x1, y1 = screen(self.x, self.y)
+        x2, y2 = screen(self.x + self.length_x, self.y + self.length_y)
         # self._canvas.delete('cannon')
         # self.cannon = self._canvas.create_line(x1, y1, x2, y2, width=7, fill='black', tag='cannon')
         self._canvas.coords(self.cannon, x1, y1, x2, y2)
@@ -94,8 +108,19 @@ class Cannon:
             self._canvas.itemconfig(self.cannon, fill='black')
 
     def shoot(self, x, y):
-        # экранные координаты даны
         self.target(x, y)
-        Vx = self.length_x
-        Vy = self.length_y
-        return Ball(self.x + self.length_x, self.y + self.length_y, shell_radius, 10, 10, self._canvas)
+        Vx = self.length_x*2.3
+        Vy = self.length_y*2.3
+        return Ball(self.x + self.length_x, self.y + self.length_y, shell_radius, Vx, Vy, self._canvas)
+
+    def hit_check(self, shell):
+        return (shell.x - self.x)**2 + (shell.y - self.y)**2 <= (self.r + shell.r)**2
+
+    def redraw(self, x, y):
+        self._canvas.delete('cannon')
+        self.cannon = self._canvas.create_line(screen(x, y),
+                                               screen(x + self.length_x, y + self.length_y),
+                                               width=7, fill='black', tag='cannon')
+        self.cannon_body = self._canvas.create_oval(screen(x - self.r, y - self.r),
+                                                    screen(x + self.r, y + self.r),
+                                                    fill='black', tag='cannon')
